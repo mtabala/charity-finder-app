5656ef45a7a4d0b04e19ca99cba7956ee56cf583
kabdulka <kenabdulka@gmail.com>
2023-03-16 19:19:33 -0700

    add organization route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
+const organizationRoutes = require("./routes/organization")
 const PORT = 9090;
 
 const organizationRoutes = require("./routes/organization.js")
