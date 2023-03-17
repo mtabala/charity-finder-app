9d09e19ede399a5963327d2d7ad16caab82056c8
kabdulka <kenabdulka@gmail.com>
2023-03-16 19:10:08 -0700

    add middleware to index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 const PORT = 9090;
+
+const organizationRoutes = require("./routes/organization.js")
+
+app.use(cors({origin: CORS_ORIGIN}))
+app.use(express.static('public'));
+
+app.use('/organization', organizationRoutes);
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
