1d710d116824ba70cf010f02c4d826c54c74df0c
Masha <mariia.tabala@gmail.com>
2023-03-16 19:14:59 -0700

    create a function to read organizations and get organizations

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { v4: uuidv4 } = require("uuid");
+const fs = require ("fs");
+
+//function to read organizations
+
+function readOrganizations () {
+    const organizationsFile = fs.readFileSync("./data/charity-list.json");
+    const organizationsData = JSON.parse(organizationsFile);
+    return organizationsData;
+}
+
+//get organizations 
+router.get("/", (req,res) => {
+    const organizations = readOrganizations;
+    const organizationsData = organizations.map ((organization) => {
+        return {
+            id: organization.id, 
+            charity: organization.charity,
+            description: organization.description,
+            field: organization.field,
+            image: organization.image,
+        }
+    })
+    res.status(200).json(organizationsData);
+})
